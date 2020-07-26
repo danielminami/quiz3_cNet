a02ed4d2a6321d29ee3bba008101f9da987d530e
Manuel Sanchez <manuel.sanchezm@gmail.com>
2020-07-26 10:11:30 -0400

    Removed test line

--- quiz3/Program.cs
+++ quiz3/Program.cs
@@ -16,7 +16,6 @@ namespace quiz3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PurchaseForm());
             Application.Run(new UserLogin());
         }
     }
